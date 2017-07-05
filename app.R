9e53dd77bb17cef5d8099d1a4194858b6c3c24d7
0ldm4j0r <0ldm4j0r@gmail.com>
2017-07-05 10:49:31 -0400

    Removing code that isn't needed for working with a pre-initialized config.

--- app.R
+++ app.R
@@ -2,24 +2,6 @@ library(shiny)
 library(Kaphi)
 library(phylocanvas)
 
-distributions <- list(
-  "exp" = list(
-    "rate" = list("Lower" = 0, "Upper" = Inf, "Default" = 1)
-  ),
-  "gamma" = list(
-    "rate" = list("Lower" = 0, "Upper" = Inf, "Default" = 1),
-    "shape" = list("Lower" = 0, "Upper" = Inf, "Default" = 1)
-  ),
-  "lnorm" = list(
-    "mean" = list("Lower" = 0, "Upper" = Inf, "Default" = 1),
-    "sd" = list("Lower" = 0, "Upper" = Inf, "Default" = 1)
-  ),
-  "norm" = list(
-    "mean" = list("Lower" = 0, "Upper" = Inf, "Default" = 1),
-    "sd" = list("Lower" = 0, "Upper" = Inf, "Default" = 1)
-  )
-)
-
 models <- list(
   "Coalescent" = list(
     "Constant Coalescent"
@@ -45,99 +27,6 @@ models <- list(
   )
 )
 
-parameters <- list(
-  "Constant Coalescent" = list(
-    "Ne.tau"
-  ),
-  "SIRD" = list(
-    "beta",
-    "gamma",
-    "mu"
-  ),
-  "SIRND" = list(
-    "beta",
-    "gamma"
-  ),
-  "SEIR" = list(
-    "beta",
-    "gamma",
-    "mu", 
-    "alpha" 
-  ),
-  "SIS" = list(
-    "beta",
-    "gamma",
-    "mu"
-  ),
-  "Yule" = list(
-    "lambda"
-  ), 
-  "Birth-Death" = list(
-    "lambda",
-    "mu" 
-  ),
-  "BiSSE" = list(
-    "lambda0",
-    "lambda1",
-    "mu0",
-    "mu1",
-    "q01",
-    "q10"
-  ),
-  "MuSSE" = list(
-    "lambda1", 
-    "lambda2",
-    "lambda3", 
-    "mu1",
-    "mu2",
-    "mu3",
-    "q12",
-    "q13",
-    "q21",
-    "q23",
-    "q31",
-    "q32"
-  ),
-  "QuaSSE" = list(
-    "lambda",
-    "mu",
-    "char"
-  ),
-  "GeoSSE" = list(
-    "sA",
-    "sB",
-    "sAB",
-    "xA",
-    "xB",
-    "dA",
-    "dB"
-  ),
-  "BiSS-ness" = list(
-    "lambda0",
-    "lambda1",
-    "mu0",
-    "mu1",
-    "q01",
-    "q10",
-    "p0c",
-    "p0a",
-    "p1c",
-    "p1a" 
-  ),
-  "ClaSSE" = list(
-    "lambda111",
-    "lambda112",
-    "lambda122",
-    "lambda211",
-    "lambda212",
-    "lambda222",
-    "mu1",
-    "mu2",
-    "q12",
-    "q21"
-  )
-)
-
 ui <- fluidPage(
   
   # Page title
@@ -160,35 +49,15 @@ ui <- fluidPage(
         actionButton(inputId = "processString", label = "Process String"),
         actionButton(inputId = "processFile", label = "Process File")
       ),
-      # Row for configuration creation
+      # Row for model selection and configuration form download
       fluidRow(
-        h3(strong(em("SMC Settings Initialization"))),
-        numericInput(inputId = "particleNumber", label = "Number of Particles", value = 1000),
-        numericInput(inputId = "sampleNumber", label = "Number of Samples", value = 5),
-        numericInput(inputId = "ESSTolerance", label = "Effective Sample Size (ESS) Tolerance", value = 1.5),
-        numericInput(inputId = "finalEpsilon", label = "Final Epsilon", value = 0.01),
-        numericInput(inputId = "finalAcceptanceRate", label = "Final Acceptance Rate", value = 0.015),
-        numericInput(inputId = "quality", label = "Quality", value = 0.95),
-        numericInput(inputId = "stepTolerance", label = "Step Tolerance", value = 1e-5),
-        actionButton(inputId = "initializeSMCSettings", label = "Initialize SMC Settings")
-      ),
-      # Row for model selection and initialization
-      fluidRow(
-        h3(strong(em("Model Selection and Initialization"))),
+        h3(strong(em("Model Selection and Configuration Form Download"))),
         selectInput("generalModel", "General Model", names(models)),
-        selectInput("specificModel", "Specific Model", models[[1]]),
-        tabsetPanel(
-          # Tab for priors
-          tabPanel(
-            title = "Priors",
-            uiOutput("priorsTabs")
-          ),
-          # Tab for proposals
-          tabPanel(
-            title = "Proposals",
-            uiOutput("proposalsTabs")
-          )
-        )
+        selectInput("specificModel", "Specific Model", models[[1]])
+      ),
+      # Row for configuration upload
+      fluidRow(
+        h3(strong(em("Configuration Upload")))
       ),
       # Row for running simulation
       fluidRow(
@@ -240,30 +109,6 @@ server <- function(input, output, session) {
   
   newickInput <- reactiveValues(data = NULL)
   
-  config <- list(
-    params=NA,
-    priors=list(),
-    prior.densities=list(),
-    proposals=list(),
-    proposal.densities=list(),
-    model=NA,
-    
-    # SMC settings
-    nparticle=1000,
-    nsample=5,
-    ess.tolerance=1.5,
-    final.epsilon=0.01,
-    final.accept.rate=0.015,
-    quality=0.95,
-    step.tolerance=1e-5,
-    
-    # Kernel settings
-    decay.factor=0.2,
-    rbf.variance=2.0,
-    sst.control=1.0,
-    norm.mode='MEAN'
-  )
-  
   # Reading tree from newick string
   observeEvent(
     input$processString,
@@ -292,68 +137,10 @@ server <- function(input, output, session) {
     phylocanvasOutput("tree", width = input$width, height = input$height)
   })
   
-  # Initializing SMC settings
-  observeEvent(
-    input$initializeSMCSettings,
-    {
-      config$nparticle <- input$particleNumber
-      config$nsample <- input$sampleNumber
-      config$ess.tolerance <- input$ESSTolerance
-      config$final.epsilon <- input$finalEpsilon
-      config$final.accept.rate <- input$finalAcceptanceRate
-      config$quality <- input$quality
-      config$step.tolerance <- input$stepTolerance
-    }
-  )
-  
   # Handling general and specific model selection
   observe({
     updateSelectInput(session, "specificModel", choices = models[[input$generalModel]])
   })
-  
-  # Displaying priors for a specific model in tabs
-  output$priorsTabs <- renderUI({
-    nTabs = length(parameters[[input$specificModel]])
-    tabs = lapply(seq_len(nTabs), function(i) {
-      tabPanel(
-        paste0(parameters[[input$specificModel]][[i]]),
-        uiOutput(paste0(input$specificModel, "Prior", parameters[[input$specificModel]][[i]]))
-      )
-    })
-    do.call(tabsetPanel, tabs)
-  })
-  
-  # Creating inputs for each specific prior
-  observe(
-    lapply(seq_len(length(parameters[[input$specificModel]])), function(i) {
-      output[[paste0(input$specificModel, "Prior", parameters[[input$specificModel]][[i]])]] <- renderUI({
-        distribution <- paste0(input$specificModel, "Prior", parameters[[input$specificModel]][[i]], "Distribution")
-        selectInput(inputId = distribution, label = "Distribution",  choices = names(distributions))
-      })
-    })
-  )
-    
-  # Displaying proposals for a specific model in tabs
-  output$proposalsTabs <- renderUI({
-    nTabs = length(parameters[[input$specificModel]])
-    tabs = lapply(seq_len(nTabs), function(i) {
-      tabPanel(
-        paste0(parameters[[input$specificModel]][[i]]),
-        uiOutput(paste0(input$specificModel, "Proposal", parameters[[input$specificModel]][[i]]))
-      )
-    })
-    do.call(tabsetPanel, tabs)
-  })
-  
-  # Creating inputs for each specific proposal
-  observe(
-    lapply(seq_len(length(parameters[[input$specificModel]])), function(i) {
-      output[[paste0(input$specificModel, "Proposal", parameters[[input$specificModel]][[i]])]] <- renderUI({
-        distribution <- paste0(input$specificModel, "Prior", parameters[[input$specificModel]][[i]], "Distribution")
-        selectInput(inputId = distribution, label = "Distribution",  choices = names(distributions))
-      })
-    })
-  )
   
 }
 
